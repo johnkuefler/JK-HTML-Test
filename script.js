f878fe77d5d0905857fb1156f5d7b23d902160d6
johnkuefler <john@dev-squared.com>
2023-01-25 12:58:32 -0600

    1 file modified

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function calculateMortgage() {
-  var loanAmount = document.getElementById("loan-amount").value;
-  var interestRate = document.getElementById("interest-rate").value / 100 / 12;
-  var loanTerm = document.getElementById("loan-term").value * 12;
-  
-  var mortgage = loanAmount * (interestRate * Math.pow(1 + interestRate, loanTerm)) / (Math.pow(1 + interestRate, loanTerm) - 1);
-  var output = "Monthly Mortgage Payment: $" + mortgage.toFixed(2);
-  
-  document.getElementById("output").innerHTML = output;
+  var loanAmount = document.getElementById('loan-amount').value;
+  var interestRate = document.getElementById('interest-rate').value / 100 / 12;
+  var loanTerm = document.getElementById('loan-term').value * 12;
+
+  var mortgage =
+    (loanAmount * (interestRate * Math.pow(1 + interestRate, loanTerm))) /
+    (Math.pow(1 + interestRate, loanTerm) - 1);
+  var output = 'Monthly Mortgage Payment: $' + mortgage.toFixed(2);
+
+  document.getElementById('output').innerHTML = output;
 }
+
+// test
